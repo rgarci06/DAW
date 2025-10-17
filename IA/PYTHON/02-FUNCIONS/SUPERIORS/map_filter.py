56537ed09d9a9ac4c90fd093823d824330f92299
rgarci06 <rgarci@insdanielblanxart.cat>
2025-10-17 17:53:15 +0200

    GOD

--- IA/PYTHON/02-FUNCIONS/SUPERIORS/map_filter.py
+++ IA/PYTHON/02-FUNCIONS/SUPERIORS/map_filter.py
@@ -5,14 +5,17 @@ filter=>agafa aquells valors que necessitem, retorna nou element iterable filter
 els retorna. 
 '''
 
-noms=["son Goku", "trunks", "vegeta", "yamcha", "krilin"]
+noms=["goku", "vegeta", "trunks", "yamcha", "krilin"]
 nums=[1,2,3,4,5,6,7,8,9]
 
-# ? map -------------------------------------------------------------------------------
+# ? map retorna objecte iterable map i aquest objecte no es por llegir--------------------------------------
 
 # variable = map(lambda, llista)
-
+nomsMaju=map(lambda nom: nom.capitalize(), noms)
+#print(list(nomsMaju))
 
 # ? filter (només retona valors que sigui True) ------------------------------------------------------------
 
 # variable = filter(lambda, llista)
+parells=filter(lambda num: num%2==0, nums)
+print(list(parells))
