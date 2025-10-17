476f08f6475b0bc3f72fbe3179628e516b8a2ede
rgarci06 <rgarci@insdanielblanxart.cat>
2025-10-17 16:21:07 +0200

    GOD

---
+++ ENTORNS DE SERVIDOR/ACT-2/ACT2_GarciaRaúl.py
@@ -0,0 +1,25 @@
+# ? ----------DATETIME-------------
+# Què ens retorni la data i hora actuals
+
+# D'aquesta data i hora actuals fes un type. Per saber quin tipus de variable retorna dita funció. 
+
+# La creació d'una data a través dels paràmetres del constructor datetime i date.
+
+# Amb la funció date que ens retorni la data d'avui.
+
+# Aquesta, data d'avui, transformar-la en una data europea amb strftime(). 
+
+# ? ----------PROGRAMA-------------
+# Preguntarem el nom de l'usuari.
+
+# Preguntarem usuari per una activitat posant el nom de l'usuari.
+
+# Preguntarem usuari per aquesta activitat quin dia l'ha de fer. El calendari ha de ser Europeu.
+
+# Si l'activitat és un nombre o una cadena buida dirà que és error i ho tornarà a preguntar.
+
+# Si usuari posa 0 a activitat o a data. Sortirà del programa.
+
+# En sortir del programa mostrarà totes les activitats i les seves dates.
+
+# I dirà adeu a l'usuari amb el seu nom!
