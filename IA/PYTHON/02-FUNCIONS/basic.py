cc40695a5f17444670d8ca86c3578a00b2fa7cb1
rgarci06 <rgarci@insdanielblanxart.cat>
2025-10-20 18:45:46 +0200

    GOD

--- IA/PYTHON/02-FUNCIONS/basic.py
+++ IA/PYTHON/02-FUNCIONS/basic.py
@@ -8,13 +8,34 @@ def passa():
     pass
 
 # paràmetres valors externs
+def suma(n1:int, n2:int)->int:
+    """Suma valors per paràmetre
 
+    Args:
+        n1 (int)
+        n2 (int)
 
-# ? args paràmetre type tuple-------------------------------------------------------
+    Returns:
+        int: resultat de la suma
+    """
+    return n1+n2
+ 
+
+# ? args paràmetres infinits retorna tuple-------------------------------------------------------
+
+def valors(*nums):
+    for num in nums:
+        return num
 
 
 # ? kwargs paràmetre type dicc per coses molt simples, clau només str ----------------------------------------------------
 
+def jugador(**kwargs):
+    return kwargs
 
 if __name__ == "__main__":
-   pass
+   
+   print(suma(2,3))
+   print(valors(2,3,4,5,6,7,8,8,9,907897,89356773567,"Xavi"))
+   print(jugador(nom="Jordan", equip="Bulls", dorsal=23))
+    
--- IA/PYTHON/02-FUNCIONS/generadors.py
+++ IA/PYTHON/02-FUNCIONS/generadors.py
@@ -6,11 +6,30 @@ Canvíem el return pel yield
 
 # ? generador normal -----------------------------------------------------------------------------
 
+def parells(num: int):
 
+    for i in range(1, num+1):
+        if i%2==0:
+            # generarem el valor i aquest ha d'anar guardat en una llista
+            yield i
 
 
 
 if __name__ == "__main__":
-    pass
-        
     
+    print("Comença el codi: ")
+    # fem el generador fent la crida de la funció aquest guardarà a dins d'un objecte iterable generator.
+    valors=parells(20)
+    print("Segueix codi...")
+    print("Segueix codi...")
+    # a mseura de codi que necessitem crida un valor determinat->next
+    n1=next(valors)
+    print("Segueix codi...")
+    print("Segueix codi...")
+    n2=next(valors)
+    print(f"El resultat de {n1}+{n2}={n1+n2}")
+    print("Segueix codi...")
+    n3=next(valors)
+    n4=next(valors)
+    print(n4)
+    print("Fi de codi")
