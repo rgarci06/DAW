85cfa565661b232d5b9688998a749b4f8ba4d45b
rgarci06 <rgarci@insdanielblanxart.cat>
2025-11-25 16:45:52 +0100

    GOD

--- DESPLEGAMENT WEB/projecte_api/app/main.py
+++ DESPLEGAMENT WEB/projecte_api/app/main.py
@@ -7,7 +7,12 @@ def hola():
     return {"message": "Raul Garcia Magro"}
 
 
-@app.get("/edat")
+@app.get("/edat1")
 def edat():
     n = datetime.strptime("2006-09-18","%Y-%m-%d").date(); a=date.today()
+    return {"edat": a.year-n.year-((a.month,a.day)<(n.month,n.day))}
+
+@app.get("/edat2")
+def edat():
+    n = datetime.strptime("1967-02-01","%Y-%m-%d").date(); a=date.today()
     return {"edat": a.year-n.year-((a.month,a.day)<(n.month,n.day))}
